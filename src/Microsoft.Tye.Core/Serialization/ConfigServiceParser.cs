a61f661577dc12f51e5ee461def598d3e81f32e2
Justin Kotalik <jukotali@microsoft.com>
2020-04-20 16:58:03 -0700

    Add support for merging configs

--- src/Microsoft.Tye.Core/Serialization/ConfigServiceParser.cs
+++ src/Microsoft.Tye.Core/Serialization/ConfigServiceParser.cs
@@ -45,6 +45,9 @@ namespace Tye.Serialization
                     case "project":
                         service.Project = YamlParser.GetScalarValue(key, child.Value);
                         break;
+                    case "yaml":
+                        service.Yaml = YamlParser.GetScalarValue(key, child.Value);
+                        break;
                     case "build":
                         if (!bool.TryParse(YamlParser.GetScalarValue(key, child.Value), out var build))
                         {
